b27b010d3700eb90291febdf6a3d01edeb0c1e43
sarah0727 <79985347+sarah0727@users.noreply.github.com>
2021-08-05 21:11:38 +0530

    Create Day0.cpp

---
+++ Day0.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main ()
+{
+	string inputString;   //CREATING A STRING VARIABLE TO HOLD THE VALUE 
+	 getline(cin,inputString);// GIVING INPUT IN THE STRING
+	 cout<<"Hello, World."<<endl;//PRINTING HELLOWORLD
+	 cout<<inputString;//PRINTING INPUTSTRING
+	 return 0;
+}
